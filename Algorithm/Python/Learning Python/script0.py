933fd3493f9e73079dc323133db1b87589eba413
KongJHong <nail_candy@163.com>
2019-05-09 21:40:55 +0800

    5-9

--- Algorithm/Python/Learning Python/script0.py
+++ Algorithm/Python/Learning Python/script0.py
@@ -1,18 +1,45 @@
 # A first Python script
-import sys
-import decimal
-from decimal import Decimal
 
-decimal.getcontext().prec=4
+class TreeNode:
+    def __init__(self,x):
+        self.val = x
+        self.left = None
+        self.right = None
 
-dec = decimal.Decimal(1)/decimal.Decimal(7)
-print(dec)
+    def __str__(self):
+        return "value:%d"%(self.val)
 
-dec = Decimal(0.1) + Decimal(0.1) + Decimal(0.1) - Decimal(0.3)
-dec = round(dec,3)
-print(dec)
+    
 
+class BinaryTree:
+    def __init__(self,root=None):
+        self.root = root
 
-L1 = [2,3,4]
-L2 = L1[:]
-print(L1 is L2)
+    def inTraverse(self,ansLst,node):
+        if node != None:
+            self.inTraverse(ansLst,node.left)
+            ansLst.append(node.val)
+            self.inTraverse(ansLst,node.right)
+        return ansLst
+    
+
+class Solution:
+    def buildTree(self, inorder, postorder) -> TreeNode:
+        if not inorder or not postorder: return None
+        
+        node = TreeNode(postorder[-1])
+        index = inorder.index(node.val)
+        node.left = self.buildTree(inorder[:index],postorder[:index])
+        node.right = self.buildTree(inorder[index+1:],postorder[index:-1])
+        
+        return node
+
+if __name__ == "__main__":
+    s = Solution()
+    inorder = [9,3,15,20,7]
+    postorder = [9,15,7,20,3]
+    node = s.buildTree(inorder,postorder)
+
+    b = BinaryTree(node)
+    lst = b.inTraverse([],b.root)
+    print(lst)
